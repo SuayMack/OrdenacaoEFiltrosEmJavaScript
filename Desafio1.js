39ca7d4f9345e9269f8cc8ec872870141d46251d
PriscilaCS <39917483+SuayMack@users.noreply.github.com>
2020-10-07 23:45:00 -0300

    update

--- Desafio1.js
+++ Desafio1.js
@@ -6,7 +6,7 @@ Ordene estes valores de acordo com o seguinte critério:
 Primeiro os Pares
 Depois os Ímpares
 Você deve exibir os pares em ordem crescente e na sequência os ímpares em ordem decrescente.
-*/
+
 
 
 let totalItems =[1,2,3,4];
@@ -15,11 +15,32 @@ var pares = item => !(item % 2)
 
 var impares = item => item % 2
 
+
 var numerosPar = totalItems.filter(pares);
 var numerosImpar = totalItems.filter(impares);
-var itensUnicos = numerosPar + numerosImpar
-let resposta = [...itensUnicos].join(" ");
+var decrescente = numerosImpar.sort(function(a, b) {
+    return b - a;
+});
+var itensUnicos = numerosPar +","+ decrescente
 
-console.log(resposta);
+console.log(itensUnicos);
 
+*/
+
+let totalItems =[1,2,3,4];
+
+var pares = item => !(item % 2)
+
+var impares = item => item % 2
+
+
+var numerosPar = totalItems.filter(pares);
+var numerosImpar = totalItems.filter(impares);
+var decrescente = numerosImpar.sort(function(a, b) {
+    return b - a;
+});
+var itensUnicos = numerosPar +","+ decrescente
+
+console.log(itensUnicos);
+console.log(totalItems)
 
